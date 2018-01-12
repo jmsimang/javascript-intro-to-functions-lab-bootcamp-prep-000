8cd131dbb1663bc52e223ab1bc6be1920e60f5f8
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-12 12:37:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string)
-    return "I can't hear you";
+    return "I can't hear you!";
   else if (string.toLowerCase() === string)
-    return ""
+    return "YES INDEED!";
+  else if (string === "I love you, Grandma!")
+    return 
 }
