3d088f719912d91389b523b6b921f6ca47f42460
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-12 12:36:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string)
+    return "I can't hear you";
+  else if (string.toLowerCase() === string)
+    return ""
+}
